ba8d1ea70f4195acbefab7b574cbae1a7bcd8c19
cssxsh <cssxsh@gmail.com>
2022-08-03 00:19:01 +0800

    build: 1.0.0-dev-2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,21 @@
 plugins {
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.serialization") version "1.6.21"
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
 
-    id("net.mamoe.mirai-console") version "2.12.0"
-    id("net.mamoe.maven-central-publish") version "0.7.1"
-    id("me.him188.kotlin-jvm-blocking-bridge") version "2.1.0-162.1"
+    id("net.mamoe.mirai-console") version "2.12.1"
+    id("me.him188.maven-central-publish") version "1.0.0-dev-3"
+    id("me.him188.kotlin-jvm-blocking-bridge") version "2.1.0-170.1"
 }
 
 group = "xyz.cssxsh.mirai"
-version = "1.0.0-dev-1"
+version = "1.0.0-dev-2"
 
 mavenCentralPublish {
     useCentralS01()
     singleDevGithubProject("cssxsh", "mirai-tts-plugin")
     licenseFromGitHubProject("AGPL-3.0", "master")
+    workingDir = System.getenv("PUBLICATION_TEMP")?.let { file(it).resolve(projectName) }
+        ?: project.buildDir.resolve("publishing-tmp")
     publication {
         artifact(tasks.getByName("buildPlugin"))
     }
@@ -24,25 +26,25 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+    api("xyz.cssxsh.baidu:baidu-aip:3.1.4") {
+        exclude(group = "org.jetbrains.kotlin")
+        exclude(group = "org.jetbrains.kotlinx")
+        exclude(group = "org.slf4j")
+    }
+    compileOnly("net.mamoe:mirai-core-utils:2.12.1")
+    compileOnly("net.mamoe:mirai-silk-converter:0.0.5")
+    //
+    testImplementation(kotlin("test"))
+    testImplementation("net.mamoe:mirai-silk-converter:0.0.5")
+}
+
 kotlin {
     explicitApi()
 }
 
 mirai {
     jvmTarget = JavaVersion.VERSION_11
-}
-
-dependencies {
-    api("xyz.cssxsh.baidu:baidu-aip:3.1.3") {
-        exclude(group = "org.jetbrains.kotlin")
-        exclude(group = "org.jetbrains.kotlinx")
-        exclude(group = "org.slf4j")
-    }
-    compileOnly("net.mamoe:mirai-core-utils:2.12.0")
-    compileOnly("net.mamoe:mirai-silk-converter:0.0.5")
-    //
-    testImplementation(kotlin("test", "1.6.21"))
-    testImplementation("net.mamoe:mirai-silk-converter:0.0.5")
 }
 
 tasks {
