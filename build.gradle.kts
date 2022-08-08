530ddc66fa52edd832a308bfa2c79797dc5ba135
cssxsh <cssxsh@gmail.com>
2022-08-08 12:33:29 +0800

    change: publish info

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ version = "1.0.1"
 mavenCentralPublish {
     useCentralS01()
     singleDevGithubProject("cssxsh", "mirai-tts-plugin")
-    licenseFromGitHubProject("AGPL-3.0", "master")
+    licenseFromGitHubProject("AGPL-3.0")
     workingDir = System.getenv("PUBLICATION_TEMP")?.let { file(it).resolve(projectName) }
         ?: project.buildDir.resolve("publishing-tmp")
     publication {
