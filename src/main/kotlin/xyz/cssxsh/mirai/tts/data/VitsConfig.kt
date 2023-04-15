7126145ec7fb22c81421713021df8f4ea6e70ea8
cssxsh <cssxsh@gmail.com>
2023-04-15 21:56:31 +0800

    fix: Vits.yml

--- src/main/kotlin/xyz/cssxsh/mirai/tts/data/VitsConfig.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/tts/data/VitsConfig.kt
@@ -1,6 +1,8 @@
 package xyz.cssxsh.mirai.tts.data
 
+import kotlinx.coroutines.*
 import net.mamoe.mirai.console.data.*
+import net.mamoe.mirai.console.plugin.jvm.*
 import net.mamoe.mirai.console.util.*
 
 public object VitsConfig : ReadOnlyPluginConfig("Vits") {
@@ -13,6 +15,11 @@ public object VitsConfig : ReadOnlyPluginConfig("Vits") {
     override fun onInit(owner: PluginDataHolder, storage: PluginDataStorage) {
         super.onInit(owner, storage)
 
-        System.setProperty("xyz.cssxsh.mirai.vits.moegoe", moegoe)
+        if (owner is JvmPlugin) {
+            owner.launch {
+                delay(3_000)
+                System.setProperty("xyz.cssxsh.mirai.vits.moegoe", moegoe)
+            }
+        }
     }
 }
